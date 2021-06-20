ab53de25e1a7176435425d3d89e22c776ad6f978
Jack Davenport <jack.davenport5011@gmail.com>
2021-06-20 11:12:16 +0100

    Added handler on server for same state requests

--- api/socket.js
+++ api/socket.js
@@ -18,15 +18,21 @@ module.exports = async function createServer(server, fan) {
         // add handler for socket requesting to change the
         // fan state
         socket.on("switch", (state) => {
+            // check if state is different to current state
             if(fanState != state) {
                 fan.write(state ? 1 : 0, (err) => {
                     if(err) {
                         socket.emit("error");
                     } else {
+                        // broadcast state change to all clients
                         fanState = state;
                         io.emit("state", state);
                     }
                 });
+            } else {
+                // if the state is the same, just send back
+                // the current state
+                socket.emit("state", state);
             }
         });
 
